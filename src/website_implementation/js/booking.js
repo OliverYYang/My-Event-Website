6014b32768ea96faa441418cfef4662721b55e5f
oliveryyang <oliveryyang@hotmail.com>
2023-10-21 16:32:40 +1000

    add the function seat click event
    This functions allow users to click unoccupied seats inside the container to select or deselect it.

--- src/website_implementation/js/booking.js
+++ src/website_implementation/js/booking.js
@@ -3,3 +3,15 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+let ticketprice = +movieSelect.value;
+
+
+// Seat click event
+container.addEventListener('click', (e) => {
+    if(e.target.classList.contains('seat')&& !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+});
