cf517b4f075dba916b5f69929f350917c2e32f9f
oliveryyang <oliveryyang@hotmail.com>
2023-10-23 08:34:59 +1000

    search button functions
    add search bar functions

--- src/website_implementation/js/index.js
+++ src/website_implementation/js/index.js
@@ -1,25 +1,42 @@
 //arrow
-const arrows = document.querySelectorAll(".arrow");
-const movieLists = document.querySelectorAll(".movie-list");
+// const arrows = document.querySelectorAll(".arrow");
+// const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, i) => {
-  const itemNumber = movieLists[i].querySelectorAll("img").length;
-  let clickCounter = 0;
-  arrow.addEventListener("click", () => {
-    const ratio = Math.floor(window.innerWidth / 270);
-    clickCounter++;
-    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      movieLists[i].style.transform = `translateX(${
-        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
-      }px)`;
-    } else {
-      movieLists[i].style.transform = "translateX(0)";
-      clickCounter = 0;
-    }
+// arrows.forEach((arrow, i) => {
+//   const itemNumber = movieLists[i].querySelectorAll("img").length;
+//   let clickCounter = 0;
+//   arrow.addEventListener("click", () => {
+//     const ratio = Math.floor(window.innerWidth / 270);
+//     clickCounter++;
+//     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+//       movieLists[i].style.transform = `translateX(${
+//         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+//       }px)`;
+//     } else {
+//       movieLists[i].style.transform = "translateX(0)";
+//       clickCounter = 0;
+//     }
+//   });
+
+//   console.log(Math.floor(window.innerWidth / 270));
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var searchToggle = document.querySelector('.search-toggle');
+  var popup = document.getElementById('searchPopup');
+
+  searchToggle.addEventListener('click', function() {
+      if (getComputedStyle(popup).display === "none") {
+          popup.style.display = "block";
+      } else {
+          popup.style.display = "none";
+      }
   });
-
-  console.log(Math.floor(window.innerWidth / 270));
 });
+
+
+
 
 
 
