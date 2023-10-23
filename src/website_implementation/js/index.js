a626defb2fc065be22d632cb6977976e6081e87a
oliveryyang <oliveryyang@hotmail.com>
2023-10-23 22:17:02 +1000

    delete the function of arrow

--- src/website_implementation/js/index.js
+++ src/website_implementation/js/index.js
@@ -1,25 +1,25 @@
 //arrow
-// const arrows = document.querySelectorAll(".arrow");
-// const movieLists = document.querySelectorAll(".movie-list");
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 
-// arrows.forEach((arrow, i) => {
-//   const itemNumber = movieLists[i].querySelectorAll("img").length;
-//   let clickCounter = 0;
-//   arrow.addEventListener("click", () => {
-//     const ratio = Math.floor(window.innerWidth / 270);
-//     clickCounter++;
-//     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-//       movieLists[i].style.transform = `translateX(${
-//         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
-//       }px)`;
-//     } else {
-//       movieLists[i].style.transform = "translateX(0)";
-//       clickCounter = 0;
-//     }
-//   });
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
 
-//   console.log(Math.floor(window.innerWidth / 270));
-// });
+  console.log(Math.floor(window.innerWidth / 270));
+});
 
 
 document.addEventListener('DOMContentLoaded', function() {
